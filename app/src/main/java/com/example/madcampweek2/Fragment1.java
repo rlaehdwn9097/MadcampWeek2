311c9b417973fb2d41d509073fbcf6a88e2e2309
rlaehdwn9097 <rlaehdwn9097@naver.com>
2021-01-12 04:43:20 +0900

    초기 완성

--- app/src/main/java/com/example/madcampweek2/Fragment1.java
+++ app/src/main/java/com/example/madcampweek2/Fragment1.java
@@ -3,12 +3,15 @@ package com.example.madcampweek2;
 import android.animation.ObjectAnimator;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +21,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,8 +34,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Array;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -47,7 +55,9 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
     private View rootView;
     private FloatingActionButton fab;
     private FloatingActionButton itemFab;
+    private FloatingActionButton refreshFab;
     private ListView listView;
+    private SearchView searchView;
     private ArrayList<String> names = new ArrayList<String>();
     private ContactList contactList = ContactList.getInstance();
     private String name;
@@ -55,9 +65,8 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
     private String number;
     private String email;
     private FirebaseUser firebaseUser;
-    private ArrayAdapter<String> listViewAdapter;
-
-
+    //private ListViewAdapter listViewAdapter;
+    private ArrayAdapter<String > listViewAdapter;
 
     public Fragment1() {
         // Required empty public constructor
@@ -68,30 +77,42 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        if (savedInstanceState != null) {
-            String uid = getArguments().getString("uid");
-            Toast.makeText(getContext(), "uid = " + uid, Toast.LENGTH_SHORT).show();
-        }
 
         rootView = inflater.inflate(R.layout.fragment_1, container, false);
         fab = (FloatingActionButton) rootView.findViewById(R.id.mainFab);
         itemFab = (FloatingActionButton) rootView.findViewById(R.id.insertfab);
+        refreshFab = (FloatingActionButton) rootView.findViewById(R.id.refreshfab);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         userid = firebaseUser.getUid();
 
+
         fab.setOnClickListener(this);
         itemFab.setOnClickListener(this);
+        refreshFab.setOnClickListener(this);
 
         getNames(names);
 
         listView = (ListView) rootView.findViewById(R.id.f1_listview);
+        //searchView = (SearchView) rootView.findViewById(R.id.search_bar);
 
         listViewAdapter = new ArrayAdapter<String>(
                 getActivity(),
                 android.R.layout.simple_list_item_1,
                 names
         );
+
+/*
+
+        listViewAdapter = new ListViewAdapter(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                ContactList.getContactList()
+        );
+
+ */
+
+
 
         listView.setAdapter(listViewAdapter);
 
@@ -101,27 +122,55 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
                 name = contactList.get(position).getName();
                 number = contactList.get(position).getNumber();
                 email = contactList.get(position).getEmail();
+
                 show(position, false);
             }
         });
 
+
+        /*
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                listViewAdapter.getFilter().filter(query);
+                //refreshFragment();
+                getNames(names);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                listViewAdapter.getFilter().filter(newText);
+                //ment();
+                getNames(names);
+                return false;
+            }
+        });
+
+         */
+
+
         return rootView;
     }
-
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
+
+            case R.id.refreshfab:
+                refreshFragment();
+                break;
+
             case R.id.mainFab:
 
                 if(!isOpen){
-                    //ObjectAnimator.ofFloat(itemFab, "translationY", -400f).start();
                     ObjectAnimator.ofFloat(itemFab, "translationY", -200f).start();
+                    ObjectAnimator.ofFloat(refreshFab, "translationY", -400f).start();
                     isOpen = true;
                 }
                 else{
-                    //ObjectAnimator.ofFloat(itemFab, "translationY", 0f).start();
                     ObjectAnimator.ofFloat(itemFab, "translationY", 0f).start();
+                    ObjectAnimator.ofFloat(refreshFab, "translationY", 0f).start();
                     isOpen = false;
                 }
                 break;
@@ -153,6 +202,10 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
             numberText.setText(number);
             emailText.setText(email);
 
+            AlertDialog dialog = builder.create();
+
+            dialog.show();
+
             confirm.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -165,6 +218,10 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
                     Contact obj = new Contact(0, userid, name, number, email);
                     PUD pud = new PUD();
                     pud.updateContact(id, obj, listViewAdapter);
+                    getNames(names);
+
+                    refreshFragment();
+                    dialog.dismiss();
                 }
             });
 
@@ -174,19 +231,23 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
                     int id = ContactList.get(position).getId();
                     PUD pud = new PUD();
                     pud.deleteContact(id, listViewAdapter);
+                    getNames(names);
+
+                    refreshFragment();
+                    dialog.dismiss();
                 }
             });
 
             call.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", number = numberText.getText().toString(), null));
+                    startActivity(intent);
+                    dialog.dismiss();
                 }
             });
 
-            AlertDialog dialog = builder.create();
 
-            dialog.show();
         } else {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             LayoutInflater inflater = getLayoutInflater();
@@ -199,7 +260,9 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
 
             final Button confirm = view.findViewById(R.id.confirm_button);
             final Button delete = view.findViewById(R.id.delete_button);
+            delete.setVisibility(View.GONE);
             final Button call = view.findViewById(R.id.call_button);
+            call.setVisibility(View.GONE);
 
             name = "";
             number = "";
@@ -217,8 +280,6 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
                 public void onClick(View v) {
                     //int id = ContactList.get(position).getId();
 
-                    Toast.makeText(getContext(), userid, Toast.LENGTH_SHORT).show();
-
                     name = nameText.getText().toString();
                     number = numberText.getText().toString();
                     email = emailText.getText().toString();
@@ -227,13 +288,11 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
                     PUD pud = new PUD();
                     pud.postContact(obj, listViewAdapter);
 
-                    getFragmentManager()
-                            .beginTransaction()
-                            .detach(Fragment1.this)
-                            .attach(Fragment1.this)
-                            .commit();
+                    refreshFragment();
 
                     dialog.dismiss();
+
+                    getNames(names);
                 }
             });
 
@@ -250,13 +309,12 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
 
                 }
             });
-
-
-
         }
     }
 
+
     public void getNames(ArrayList<String> names) {
+
         JsonPlaceHolderAPI mAPI = NetRetrofit.getInstance().getService();
 
         Call<List<Contact>> getCall = mAPI.getContacts();
@@ -272,7 +330,8 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
                         if (contact.getUserid().equals(userid)) {
                             names.add(contact.getName());
                             contactList.add(contact);
-                            //contactList.sort1();
+                            names.sort(String::compareTo);
+                            contactList.sort1();
                         }
                     }
                     listViewAdapter.notifyDataSetChanged();
@@ -285,5 +344,20 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
                 Log.d(TAG, "Fail msg: " + t.getMessage());
             }
         });
+    }
+
+    public void refreshFragment() {
+        Toast.makeText(getContext(), "Fragment being refreshed.", Toast.LENGTH_SHORT).show();
+        final Handler handler = new Handler (Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getFragmentManager()
+                        .beginTransaction()
+                        .detach(Fragment1.this)
+                        .attach(Fragment1.this)
+                        .commit();
+            }
+        }, 1000);
     }
 }
--- app/src/main/java/com/example/madcampweek2/Fragment3.java
+++ app/src/main/java/com/example/madcampweek2/Fragment3.java
@@ -7,11 +7,69 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.common.TensorOperator;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class Fragment3 extends Fragment {
 
+    protected Interpreter tflite;
+    private  int imageSizeX;
+    private  int imageSizeY;
 
+    private static final float IMAGE_MEAN = 0.0f;
+    private static final float IMAGE_STD = 1.0f;
+
+    public Bitmap oribitmap,testbitmap;
+    public static Bitmap cropped;
+    Uri imageuri;
+
+    ImageView oriImage,testImage;
+    Button buverify;
+    TextView result_text;
+
+    float[][] ori_embedding = new float[1][128];
+    float[][] test_embedding = new float[1][128];
 
     public Fragment3() {
         // Required empty public constructor
@@ -22,7 +80,176 @@ public class Fragment3 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_3, container, false);
-
+        initComponents(rootView);
         return rootView;
     }
+
+
+    private void initComponents(View rootView) {
+
+        oriImage=(ImageView) rootView.findViewById(R.id.image1);
+        testImage=(ImageView)rootView.findViewById(R.id.image2);
+        buverify=(Button)rootView.findViewById(R.id.verify);
+        result_text=(TextView)rootView.findViewById(R.id.result);
+
+        try{
+            tflite=new Interpreter(loadmodelfile(getActivity()));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        oriImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),12);
+            }
+        });
+
+        testImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),13);
+            }
+        });
+
+        buverify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                double distance=calculate_distance(ori_embedding,test_embedding);
+
+                if(distance<10.0)
+                    result_text.setText("Result : Same Faces");
+                else
+                    result_text.setText("Result : Different Faces");
+
+            }
+
+        });
+
+    }
+
+    private double calculate_distance(float[][] ori_embedding, float[][] test_embedding) {
+        double sum =0.0;
+        for(int i=0;i<128;i++){
+            sum=sum+Math.pow((ori_embedding[0][i]-test_embedding[0][i]),2.0);
+        }
+        return Math.sqrt(sum);
+    }
+
+    private TensorImage loadImage(final Bitmap bitmap, TensorImage inputImageBuffer ) {
+        // Loads bitmap into a TensorImage.
+        inputImageBuffer.load(bitmap);
+
+        // Creates processor for the TensorImage.
+        int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
+        // TODO(b/143564309): Fuse ops inside ImageProcessor.
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                        .add(new ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+                        .add(getPreprocessNormalizeOp())
+                        .build();
+        return imageProcessor.process(inputImageBuffer);
+    }
+
+    private MappedByteBuffer loadmodelfile(Activity activity) throws IOException {
+        AssetFileDescriptor fileDescriptor=activity.getAssets().openFd("Qfacenet.tflite");
+        FileInputStream inputStream=new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel=inputStream.getChannel();
+        long startoffset = fileDescriptor.getStartOffset();
+        long declaredLength=fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startoffset,declaredLength);
+    }
+
+    private TensorOperator getPreprocessNormalizeOp() {
+        return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==12 && resultCode==RESULT_OK && data!=null) {
+            imageuri = data.getData();
+            try {
+                oribitmap = MediaStore.Images.Media.getBitmap(getActivity().getApplicationContext().getContentResolver(), imageuri);
+                oriImage.setImageBitmap(oribitmap);
+                face_detector(oribitmap,"original");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(requestCode==13 && resultCode==RESULT_OK && data!=null) {
+            imageuri = data.getData();
+            try {
+                testbitmap = MediaStore.Images.Media.getBitmap(getActivity().getApplicationContext().getContentResolver(), imageuri);
+                testImage.setImageBitmap(testbitmap);
+                face_detector(testbitmap,"test");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void face_detector(final Bitmap bitmap, final String imagetype){
+
+        final InputImage image = InputImage.fromBitmap(bitmap,0);
+        FaceDetector detector = FaceDetection.getClient();
+        detector.process(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<List<Face>>() {
+                            @Override
+                            public void onSuccess(List<Face> faces) {
+                                // Task completed successfully
+                                for (Face face : faces) {
+                                    Rect bounds = face.getBoundingBox();
+                                    cropped = Bitmap.createBitmap(bitmap, bounds.left, bounds.top, bounds.width(), bounds.height());
+                                    get_embaddings(cropped,imagetype);
+                                }
+                            }
+
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                            }
+                        });
+    }
+
+    public void get_embaddings(Bitmap bitmap,String imagetype){
+
+        TensorImage inputImageBuffer;
+        float[][] embedding = new float[1][128];
+
+        int imageTensorIndex = 0;
+        int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape(); // {1, height, width, 3}
+        imageSizeY = imageShape[1];
+        imageSizeX = imageShape[2];
+        DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
+
+        inputImageBuffer = new TensorImage(imageDataType);
+
+        inputImageBuffer = loadImage(bitmap,inputImageBuffer);
+
+        tflite.run(inputImageBuffer.getBuffer(),embedding);
+
+        if(imagetype.equals("original"))
+            ori_embedding=embedding;
+        else if (imagetype.equals("test"))
+            test_embedding=embedding;
+    }
+
+
+
 }
