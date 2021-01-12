f79c5c599c16007714d95d5dc9f1152e4a4e5af4
rlaehdwn9097 <rlaehdwn9097@naver.com>
2021-01-12 22:29:08 +0900

    최종

--- app/src/main/java/com/example/madcampweek2/Fragment3.java
+++ app/src/main/java/com/example/madcampweek2/Fragment3.java
@@ -1,9 +1,14 @@
 package com.example.madcampweek2;
 
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.media.Image;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,6 +38,7 @@ import com.google.mlkit.vision.face.Face;
 import com.google.mlkit.vision.face.FaceDetection;
 import com.google.mlkit.vision.face.FaceDetector;
 import com.google.mlkit.vision.face.FaceDetectorOptions;
+import com.google.mlkit.vision.face.FaceLandmark;
 
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.Interpreter;
@@ -67,6 +73,8 @@ public class Fragment3 extends Fragment {
     ImageView oriImage,testImage;
     Button buverify;
     TextView result_text;
+    ImageView oriImage_cropped;
+    ImageView testImage_cropped;
 
     float[][] ori_embedding = new float[1][128];
     float[][] test_embedding = new float[1][128];
@@ -89,6 +97,9 @@ public class Fragment3 extends Fragment {
 
         oriImage=(ImageView) rootView.findViewById(R.id.image1);
         testImage=(ImageView)rootView.findViewById(R.id.image2);
+        oriImage_cropped = (ImageView)rootView.findViewById(R.id.image3);
+        testImage_cropped=(ImageView)rootView.findViewById(R.id.image4);
+
         buverify=(Button)rootView.findViewById(R.id.verify);
         result_text=(TextView)rootView.findViewById(R.id.result);
 
@@ -122,54 +133,25 @@ public class Fragment3 extends Fragment {
             @Override
             public void onClick(View v) {
 
-                double distance=calculate_distance(ori_embedding,test_embedding);
+                double distance = calculate_distance(ori_embedding,test_embedding);
 
-                if(distance<10.0)
+                if(distance<7.0) {
+                    result_text.setTextColor(ContextCompat.getColor(getActivity(), R.color.sameFace));
                     result_text.setText("Result : Same Faces");
-                else
+                }
+                else if(distance>9.0){
+                    result_text.setTextColor(ContextCompat.getColor(getActivity(),R.color.differentFace));
                     result_text.setText("Result : Different Faces");
+                }
+                else {
+                    //result_text.setTextColor(ContextCompat.getColor(getActivity(),R.color.differentFace));
+                    result_text.setText("Result : Similar Faces");
+                }
 
             }
 
         });
 
-    }
-
-    private double calculate_distance(float[][] ori_embedding, float[][] test_embedding) {
-        double sum =0.0;
-        for(int i=0;i<128;i++){
-            sum=sum+Math.pow((ori_embedding[0][i]-test_embedding[0][i]),2.0);
-        }
-        return Math.sqrt(sum);
-    }
-
-    private TensorImage loadImage(final Bitmap bitmap, TensorImage inputImageBuffer ) {
-        // Loads bitmap into a TensorImage.
-        inputImageBuffer.load(bitmap);
-
-        // Creates processor for the TensorImage.
-        int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
-        // TODO(b/143564309): Fuse ops inside ImageProcessor.
-        ImageProcessor imageProcessor =
-                new ImageProcessor.Builder()
-                        .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
-                        .add(new ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
-                        .add(getPreprocessNormalizeOp())
-                        .build();
-        return imageProcessor.process(inputImageBuffer);
-    }
-
-    private MappedByteBuffer loadmodelfile(Activity activity) throws IOException {
-        AssetFileDescriptor fileDescriptor=activity.getAssets().openFd("Qfacenet.tflite");
-        FileInputStream inputStream=new FileInputStream(fileDescriptor.getFileDescriptor());
-        FileChannel fileChannel=inputStream.getChannel();
-        long startoffset = fileDescriptor.getStartOffset();
-        long declaredLength=fileDescriptor.getDeclaredLength();
-        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startoffset,declaredLength);
-    }
-
-    private TensorOperator getPreprocessNormalizeOp() {
-        return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
     }
 
     @Override
@@ -201,37 +183,70 @@ public class Fragment3 extends Fragment {
 
     public void face_detector(final Bitmap bitmap, final String imagetype){
 
+
         final InputImage image = InputImage.fromBitmap(bitmap,0);
         FaceDetector detector = FaceDetection.getClient();
-        detector.process(image)
-                .addOnSuccessListener(
-                        new OnSuccessListener<List<Face>>() {
-                            @Override
-                            public void onSuccess(List<Face> faces) {
-                                // Task completed successfully
-                                for (Face face : faces) {
-                                    Rect bounds = face.getBoundingBox();
-                                    cropped = Bitmap.createBitmap(bitmap, bounds.left, bounds.top, bounds.width(), bounds.height());
-                                    get_embaddings(cropped,imagetype);
-                                }
-                            }
 
-                        })
-                .addOnFailureListener(
-                        new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                // Task failed with an exception
-                                Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_LONG).show();
-                            }
-                        });
+        detector.process(image)
+                .addOnSuccessListener(new OnSuccessListener<List<Face>>() {
+                    @Override
+                    public void onSuccess(List<Face> faces) {
+                        Face face = faces.get(0);
+                        float eulerZ = face.getHeadEulerAngleZ();
+                        Matrix matrix = new Matrix();
+                        matrix.preRotate(eulerZ);
+                        Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+
+                        final InputImage image2 = InputImage.fromBitmap(rotatedBitmap,0);
+
+                        detector.process(image2)
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<List<Face>>() {
+                                            @Override
+                                            public void onSuccess(List<Face> faces) {
+                                                // Task completed successfully
+                                                for (Face face : faces) {
+                                                    Rect bounds = face.getBoundingBox();
+                                                    cropped = Bitmap.createBitmap(rotatedBitmap, bounds.left, bounds.top, bounds.width(), bounds.height());
+                                                    if(imagetype == "original"){
+                                                        oriImage_cropped.setImageBitmap(cropped);
+                                                    }
+                                                    else{
+                                                        testImage_cropped.setImageBitmap(cropped);
+                                                    }
+                                                    get_embaddings(cropped,imagetype);
+                                                }
+                                            }
+
+                                        })
+                                .addOnFailureListener(
+                                        new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                // Task failed with an exception
+                                                Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                                            }
+                                        });
+
+                    }
+
+                });
+
+    }
+
+    private double calculate_distance(float[][] ori_embedding, float[][] test_embedding) {
+        double sum =0.0;
+        for(int i=0;i<128;i++){
+            sum=sum+Math.pow((ori_embedding[0][i]-test_embedding[0][i]),2.0);
+        }
+        return Math.sqrt(sum);
     }
 
     public void get_embaddings(Bitmap bitmap,String imagetype){
 
         TensorImage inputImageBuffer;
         float[][] embedding = new float[1][128];
-
+        //bitmap = rotateCW(bitmap);
         int imageTensorIndex = 0;
         int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape(); // {1, height, width, 3}
         imageSizeY = imageShape[1];
@@ -250,6 +265,34 @@ public class Fragment3 extends Fragment {
             test_embedding=embedding;
     }
 
+    private TensorImage loadImage(final Bitmap bitmap, TensorImage inputImageBuffer ) {
+        // Loads bitmap into a TensorImage.
+        inputImageBuffer.load(bitmap);
+
+        // Creates processor for the TensorImage.
+        int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
+        // TODO(b/143564309): Fuse ops inside ImageProcessor.
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                        .add(new ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+                        .add(getPreprocessNormalizeOp())
+                        .build();
+        return imageProcessor.process(inputImageBuffer);
+    }
+
+    private MappedByteBuffer loadmodelfile(Activity activity) throws IOException {
+        AssetFileDescriptor fileDescriptor=activity.getAssets().openFd("Qfacenet.tflite");
+        FileInputStream inputStream=new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel=inputStream.getChannel();
+        long startoffset = fileDescriptor.getStartOffset();
+        long declaredLength=fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startoffset,declaredLength);
+    }
+
+    private TensorOperator getPreprocessNormalizeOp() {
+        return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
+    }
 
 
 }
